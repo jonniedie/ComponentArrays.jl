33c9644c3da0c8a125f25ecf285e462f5c71b135
Jonnie Diegelman <jonniediegelman@gmail.com>
2020-04-23 21:06:02 -0400

    ughhhh

--- build/show.jl
+++
@@ -1,8 +0,0 @@
-function Base.show(io::IO, ::MIME"text/plain", x::CVector)
-    print(io, "CArray{$(eltype(x))}$(NamedTuple(x))")
-    return nothing
-end
-function Base.show(io::IO, x::CVector)
-    print(io, NamedTuple(x))
-    return nothing
-end
