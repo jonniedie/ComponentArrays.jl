7dcf89e2d8eaccd7eb49de0d919b651588237f91
Yingbo Ma <mayingbo5@gmail.com>
2020-06-12 12:20:26 -0400

    Add at inbounds and propagate_inbounds

--- src/set_get.jl
+++ src/set_get.jl
@@ -50,16 +50,17 @@ Access ```.data``` field of a ```ComponentArray```, which contains the array tha
     ComponentIndex(getproperty(IdxMap, s))
 
 # Get ComponentArray index
-@inline Base.getindex(x::ComponentArray, idx::FlatIdx...) = Base.maybeview(getdata(x), idx...)
-@inline Base.getindex(x::ComponentArray, ::Colon) = @view getdata(x)[:]
+
+Base.@propagate_inbounds Base.getindex(x::ComponentArray, idx::FlatIdx...) = Base.maybeview(getdata(x), idx...)
+Base.@propagate_inbounds Base.getindex(x::ComponentArray, ::Colon) = @view getdata(x)[:]
 @inline Base.getindex(x::ComponentArray, ::Colon...) = x
-@inline Base.getindex(x::ComponentArray, idx...) = getindex(x, toval.(idx)...)
+Base.@propagate_inbounds Base.getindex(x::ComponentArray, idx...) = getindex(x, toval.(idx)...)
 @inline Base.getindex(x::ComponentArray, idx::Val...) = _getindex(x, idx...)
 
 # Set ComponentArray index
-@inline Base.setindex!(x::ComponentArray, v, idx::FlatIdx...) = setindex!(getdata(x), v, idx...)
-@inline Base.setindex!(x::ComponentArray, v, ::Colon) = setindex!(getdata(x), v, :)
-@inline Base.setindex!(x::ComponentArray, v, idx...) = setindex!(x, v, toval.(idx)...)
+Base.@propagate_inbounds Base.setindex!(x::ComponentArray, v, idx::FlatIdx...) = setindex!(getdata(x), v, idx...)
+Base.@propagate_inbounds Base.setindex!(x::ComponentArray, v, ::Colon) = setindex!(getdata(x), v, :)
+Base.@propagate_inbounds Base.setindex!(x::ComponentArray, v, idx...) = setindex!(x, v, toval.(idx)...)
 @inline Base.setindex!(x::ComponentArray, v, idx::Val...) = _setindex!(x, v, idx...)
 
 
@@ -74,11 +75,13 @@ Access ```.data``` field of a ```ComponentArray```, which contains the array tha
     inds = map(i -> i.idx, ci)
     axs = map(i -> i.ax, ci)
     axs = remove_nulls(axs...)
-    return :(Base.@_inline_meta; ComponentArray(Base.maybeview(getdata(x), $inds...), $axs...))
+    # the index must be valid after computing `ci`
+    :(Base.@_inline_meta; @inbounds ComponentArray(Base.maybeview(getdata(x), $inds...), $axs...))
 end
 
 @generated function _setindex!(x::ComponentArray, v, idx...)
     ci = getindex.(getaxes(x), getval.(idx))
     inds = map(i -> i.idx, ci)
-    return :(Base.@_inline_meta; setindex!(getdata(x), v, $inds...))
+    # the index must be valid after computing `ci`
+    return :(Base.@_inline_meta; @inbounds setindex!(getdata(x), v, $inds...))
 end
