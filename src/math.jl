55af65d272b8271e74184b292280be8c54ef1ef8
Jonnie Diegelman <jonniediegelman@gmail.com>
2020-06-17 11:05:26 -0400

    Linear algebra methods to avoid indexing fallbacks

--- src/math.jl
+++ src/math.jl
@@ -11,10 +11,32 @@ Base.transpose(x::CVector) = ComponentArray(transpose(getdata(x)), FlatAxis(), g
 Base.transpose(x::CMatrix) = ComponentArray(transpose(getdata(x)), reverse(getaxes(x))...)
 Base.transpose(x::AdjointCVector) = ComponentArray(transpose(getdata(x)), (getaxes(x)[2],))
 
-Base.:(*)(x::AdjointCVector, y::AbstractArray{<:T,<:N}) where {T,N} = getdata(x)*y
+# Avoid slow linear indexing fallback
+for f in [:(*), :(/), :(\)]
+    @eval begin
+        # The normal stuff
+        Base.$f(x::ComponentArray, y::AbstractArray) = $f(getdata(x), y)
+        Base.$f(x::AbstractArray, y::ComponentArray) = $f(x, getdata(y))
+        Base.$f(x::ComponentArray, y::ComponentArray) = $f(getdata(x), getdata(y))
 
-Base.:(\)(x::CMatrix, y::AbstractVecOrMat) = getdata(x) \ y
-Base.:(/)(x::AbstractVecOrMat, y::CMatrix) = x / getdata(y)
+        # A bunch of special cases to avoid ambiguous method errors
+        Base.$f(x::ComponentArray, y::AbstractMatrix) = $f(getdata(x), y)
+        Base.$f(x::AbstractMatrix, y::ComponentArray) = $f(x, getdata(y))
+
+        Base.$f(x::ComponentArray, y::AbstractVector) = $f(getdata(x), y)
+    end
+end
+
+# Adjoint/transpose special cases
+for f in [:(*), :(/)]
+    @eval begin
+        Base.$f(x::Adjoint, y::ComponentArray) = $f(x, getdata(y))
+        Base.$f(x::Transpose, y::ComponentArray) = $f(x, getdata(y))
+
+        Base.$f(x::Adjoint{T,<:AbstractVector{T}}, y::ComponentVector) where T = $f(x, getdata(y))
+        Base.$f(x::Transpose{T,<:AbstractVector{T}}, y::ComponentVector) where T = $f(x, getdata(y))
+    end
+end
 
 LinearAlgebra.ldiv!(x::ComponentArray, args...) = ldiv!(getdata(x), getdata.(args...)...)
 LinearAlgebra.ldiv!(Y::ComponentArray, A::Factorization, B::ComponentArray) = ldiv!(getdata(Y), A, getdata(B))
