738072e70b58e2ba24b1e329ef2676c76bfa76cc
Jonnie Diegelman <jonniediegelman@gmail.com>
2020-06-30 15:37:10 -0400

    Much faster broadcasting. Also fixes #31

--- src/broadcasting.jl
+++ src/broadcasting.jl
@@ -1,69 +1,6 @@
 const BC = Base.Broadcast
 
 
-struct CAStyle{InnerStyle<:BC.BroadcastStyle, Axes, N} <: BC.AbstractArrayStyle{N} end
-CAStyle(::InnerStyle, ::Axes, N) where {InnerStyle, Axes} = CAStyle{InnerStyle, Axes, N}()
-CAStyle(::InnerStyle, ::Type{<:Axes}, N) where {InnerStyle, Axes} = CAStyle{InnerStyle, Axes, N}()
-
-function CAStyle(::InnerStyle, ax::Axes, ::Val{N}) where {InnerStyle, Axes, N}
-    return CAStyle(InnerStyle(N), ax, N)
-end
-
-
-function BC.BroadcastStyle(::CAStyle{<:In1, <:Ax1, <:N1}, ::CAStyle{<:In2, <:Ax2, <:N2}) where {In1, Ax1, N1, In2, Ax2, N2}
-    Ax, N = fill_flat(Ax1, Ax2, N1, N2)
-    inner_style = BC.BroadcastStyle(In1(), In2())
-    if inner_style isa BC.Unknown
-        return CAStyle(BC.DefaultArrayStyle{N}(), Ax, N)
-    else
-        return CAStyle(inner_style, Ax, N)
-    end
-end
-function BC.BroadcastStyle(::CAStyle{In, Ax, N1}, style::BC.DefaultArrayStyle{N2}) where {In, Ax, N1, N2}
-    return BC.DefaultArrayStyle{max(N1, N2)}()
-end
-function BC.BroadcastStyle(CAS::CAStyle{In, Ax, N1}, style::BC.DefaultArrayStyle{0}) where {In, Ax, N1}
-    return CAS
-end
-function BC.BroadcastStyle(::CAStyle{In, Ax, N}, style::Style) where {In, Ax, N, Style<:BC.AbstractArrayStyle}
-    return BC.BroadcastStyle(In(), Style())
-end
-
-
-function Base.BroadcastStyle(::Type{<:ComponentArray{T, N, A, Axes}}) where {T, A, N, Axes}
-    return CAStyle(Base.BroadcastStyle(A), Axes, ndims(A))
-end
-function Base.BroadcastStyle(A::Type{<:Adjoint{T, <:ComponentArray}}) where {T}
-    return CAStyle(Base.BroadcastStyle(Adjoint{T,A}), getaxes(A), ndims(A))
-end
-function Base.BroadcastStyle(A::Type{<:Transpose{T, <:ComponentArray}}) where {T}
-    return CAStyle(Base.BroadcastStyle(Transpose{T,A}), getaxes(A), ndims(A))
-end
-
-function Base.BroadcastStyle(::CAStyle{InnerStyle, Axes, N}, bc::BC.Broadcasted) where {InnerStyle, Axes, N}
-    return CAStyle(Base.BroadcastStyle(InnerStyle(), bc), Axes, N)
-end
-
-
-Base.convert(::Type{<:BC.Broadcasted{Nothing}}, bc::BC.Broadcasted{<:CAStyle,<:Axes,<:F,<:Args}) where {Axes,F,Args} = getdata(bc)
-
-getdata(bc::BC.Broadcasted{<:CAStyle}) = BC.broadcasted(bc.f, map(getdata, bc.args)...)
-getdata(x::Adjoint) = getdata(x.parent)'
-getdata(x::Transpose) = transpose(getdata(x.parent))
-
-
-function Base.similar(bc::BC.Broadcasted{<:CAStyle{InnerStyle, Axes, N}}, args...) where {InnerStyle, Axes, N}
-    return ComponentArray{Axes}(similar(BC.Broadcasted{InnerStyle}(bc.f, map(getdata, bc.args), bc.axes), args...))
-end
-
-function Base.copy(bc::BC.Broadcasted{<:CAStyle{InnerStyle, Axes, N}}) where {InnerStyle, Axes,  N}
-    return ComponentArray{Axes}(Base.copy(BC.broadcasted(bc.f, map(getdata, bc.args)...)))
-end
-
-# From https://github.com/JuliaArrays/OffsetArrays.jl/blob/master/src/OffsetArrays.jl
-Base.dataids(A::ComponentArray) = Base.dataids(parent(A))
-Broadcast.broadcast_unalias(dest::ComponentArray, src::ComponentArray) = parent(dest) === parent(src) ? src : Broadcast.unalias(dest, src)
-
 # Helper for extruding axes
 function fill_flat(Ax1, Ax2, N1, N2)
     if N1<N2
@@ -90,3 +27,81 @@ function fill_flat(Ax::VarAxes, N)
     end
     return axs
 end
+
+
+struct CAStyle{InnerStyle<:BC.BroadcastStyle, Axes, N} <: BC.AbstractArrayStyle{N} end
+CAStyle(::InnerStyle, ::Axes, N) where {InnerStyle, Axes} = CAStyle{InnerStyle, Axes, N}()
+CAStyle(::InnerStyle, ::Type{<:Axes}, N) where {InnerStyle, Axes} = CAStyle{InnerStyle, Axes, N}()
+
+function CAStyle(::InnerStyle, ax::Axes, ::Val{N}) where {InnerStyle, Axes, N}
+    return CAStyle(InnerStyle(N), ax, N)
+end
+
+
+function BC.BroadcastStyle(::CAStyle{<:In1, <:Ax1, <:N1}, ::CAStyle{<:In2, <:Ax2, <:N2}) where {In1, Ax1, N1, In2, Ax2, N2}
+    ax, N = fill_flat(Ax1, Ax2, N1, N2)
+    inner_style = BC.BroadcastStyle(In1(), In2())
+    if inner_style isa BC.Unknown
+        inner_style = BC.DefaultArrayStyle{N}()
+    end
+    return CAStyle(inner_style, ax, N)
+end
+function BC.BroadcastStyle(::CAStyle{In, Ax, N1}, ::Style) where Style<:BC.DefaultArrayStyle{N2} where {In, Ax, N1, N2}
+    N = max(N1, N2)
+    ax = fill_flat(Ax, max(N1, N2))
+    inner_style = BC.BroadcastStyle(In(), Style())
+    return CAStyle(inner_style, ax, N)
+end
+function BC.BroadcastStyle(CAS::CAStyle{In, Ax, N1}, ::BC.DefaultArrayStyle{0}) where {In, Ax, N1}
+    return CAS
+end
+function BC.BroadcastStyle(CAS::CAStyle{In, Ax, N}, ::BC.DefaultArrayStyle{N}) where {In, Ax, N}
+    return CAS
+end
+function BC.BroadcastStyle(::CAStyle{In, Ax, N1}, ::Style) where Style<:BC.AbstractArrayStyle{N2} where {In, Ax, N1, N2}
+    N = max(N1, N2)
+    ax = fill_flat(Ax, max(N1, N2))
+    inner_style = BC.BroadcastStyle(In(), Style())
+    return CAStyle(inner_style, ax, N)
+end
+
+
+function Base.BroadcastStyle(::Type{<:ComponentArray{T, N, A, Axes}}) where {T, A, N, Axes}
+    return CAStyle(Base.BroadcastStyle(A), Axes, ndims(A))
+end
+function Base.BroadcastStyle(A::Type{<:Adjoint{T, <:ComponentArray}}) where {T}
+    return CAStyle(Base.BroadcastStyle(Adjoint{T,A}), getaxes(A), ndims(A))
+end
+function Base.BroadcastStyle(A::Type{<:Transpose{T, <:ComponentArray}}) where {T}
+    return CAStyle(Base.BroadcastStyle(Transpose{T,A}), getaxes(A), ndims(A))
+end
+
+function Base.BroadcastStyle(::CAStyle{InnerStyle, Axes, N}, bc::BC.Broadcasted) where {InnerStyle, Axes, N}
+    return CAStyle(Base.BroadcastStyle(InnerStyle(), bc), Axes, N)
+end
+
+
+Base.convert(::Type{<:BC.Broadcasted{Nothing}}, bc::BC.Broadcasted{<:CAStyle,<:Axes,<:F,<:Args}) where {Axes,F,Args} = getdata(bc)
+
+getdata(bc::BC.Broadcasted{<:CAStyle}) = BC.broadcasted(bc.f, map(getdata, bc.args)...)
+getdata(x::Adjoint) = getdata(x.parent)'
+getdata(x::Transpose) = transpose(getdata(x.parent))
+
+
+# function Base.similar(bc::BC.Broadcasted{<:CAStyle{InnerStyle, Axes, N}}, args...) where {InnerStyle, Axes, N}
+#     return ComponentArray{Axes}(similar(BC.Broadcasted{InnerStyle}(bc.f, map(getdata, bc.args), bc.axes), args...))
+# end
+function Base.similar(bc::BC.Broadcasted{<:CAStyle{InnerStyle, Axes, N}}, args...) where {InnerStyle, Axes, N}
+    return ComponentArray{Axes}(similar(BC.Broadcasted{InnerStyle}(bc.f, bc.args, bc.axes), args...))
+end
+
+# function Base.copy(bc::BC.Broadcasted{<:CAStyle{InnerStyle, Axes, N}}) where {InnerStyle, Axes,  N}
+#     return ComponentArray{Axes}(Base.copy(BC.broadcasted(bc.f, map(getdata, bc.args)...)))
+# end
+# function Base.copy(bc::BC.Broadcasted{<:CAStyle{InnerStyle, Axes, N}}) where {InnerStyle, Axes,  N}
+#     return ComponentArray{Axes}(Base.copy(BC.Broadcasted(InnerStyle())))
+# end
+
+# From https://github.com/JuliaArrays/OffsetArrays.jl/blob/master/src/OffsetArrays.jl
+Base.dataids(A::ComponentArray) = Base.dataids(parent(A))
+Broadcast.broadcast_unalias(dest::ComponentArray, src::ComponentArray) = parent(dest) === parent(src) ? src : Broadcast.unalias(dest, src)
--- src/if_required/staticarrays.jl
+++ src/if_required/staticarrays.jl
@@ -1,29 +1,20 @@
-# # function Base.similar(::BC.Broadcasted{<:CAStyle{T,N,<:A,<:Axes}}) where {T,N,A<:StaticArrays.StaticArray,Axes}
-# #     ComponentArray{Axes}(similar(A))
-# # end
-
 ComponentArray{A}(::UndefInitializer, ax::Axes) where {A<:StaticArrays.StaticArray,Axes<:Tuple} =
     ComponentArray(similar(A), ax...)
 
-# # function Base.similar(bc::BC.Broadcasted{<:CAStyle{T,N,<:A,<:Axes}}, ::Type{<:TT}) where {T,N,A<:StaticArrays.StaticArray,Axes,TT}
-# #     return ComponentArray{Axes}(similar(A, TT))
-# # end
+# Sometimes you just need to get things done.
+function Base.Broadcast.BroadcastStyle(::Type{s66}) where s66<:Transpose{T,s19} where s19<:ComponentArray{s62,s63,s64,s65} where s65<:Tuple{Vararg{AbstractAxis,N}} where s64<:StaticArrays.StaticArray{S,s62,N} where S<:Tuple where s63<:N where s62<:T where {T, N}
+    return CAStyle(StaticArrays.StaticArrayStyle{2}(), getaxes(s19), 2)
+end
+function Base.Broadcast.BroadcastStyle(::Type{s66}) where s66<:Adjoint{T,s19} where s19<:ComponentArray{s62,s63,s64,s65} where s65<:Tuple{Vararg{AbstractAxis,N}} where s64<:StaticArrays.StaticArray{S,s62,N} where S<:Tuple where s63<:N where s62<:T where {T, N}
+    return CAStyle(StaticArrays.StaticArrayStyle{2}(), getaxes(s19), 2)
+end
 
-# # StaticComponentArray{T,N,A,Axes} = ComponentArray{T,N,<:A,Axes} where {A<:StaticArrays.StaticArray}
+# This is pretty unideal. It gets the answer right, but it allocates in the process. Need to
+# figure this out because what's the point of StaticArrays if they are going to allocate?
+function Base.copy(bc::Base.Broadcast.Broadcasted{Style,<:Ax,<:F,s19}) where {s19<:Tuple, Axes, Ax, F, Style<:CAStyle{s18,<:s17,<:s12}} where {s12, s17, s18<:StaticArrays.StaticArrayStyle}
+    args = map(getdata, bc.args)
+    # # style = BC.combine_styles(BC.BroadcastStyle.(typeof.(args))...)
+    return ComponentArray{s17}(Base.copy(BC.broadcasted(bc.f, args...)))
+end
 
-# # function Base.BroadcastStyle(A::Type{<:Adjoint{T, <:ComponentArray{<:T,<:N,<:StaticArrays.StaticArray,<:Axes}}}) where {T,N,Axes}
-# #     return CAStyle(Base.BroadcastStyle(Adjoint{T,A}), getaxes(A), 2)
-# # end
-# # function Base.BroadcastStyle(A::Type{<:Transpose{T, <:ComponentArray{<:T,<:N,<:StaticArrays.StaticArray,<:Axes}}}) where {T,N,Axes}
-# #     return CAStyle(Base.BroadcastStyle(Transpose{T,A}), getaxes(A), 2)
-# # end
 
-
-# # Sometimes you just need to get things done.
-# function Base.Broadcast.BroadcastStyle(::Type{s19}) where s19<:Adjoint{T,s18} where s18<:ComponentArray{s12,s20,s21,s22} where s22<:Tuple{Vararg{AbstractAxis,N} where N} where s21<:StaticArray{S,s12,N} where S<:Tuple where N where s20<:N where s12<:T where {T, N}
-#     return CAStyle(Base.BroadcastStyle(Adjoint{T,A}), getaxes(A), 2)
-# end
-
-# function Base.Broadcast.BroadcastStyle(::Type{s19}) where s19<:Transpose{T,s18} where s18<:ComponentArray{s12,s20,s21,s22} where s22<:Tuple{Vararg{AbstractAxis,N} where N} where s21<:StaticArray{S,s12,N} where S<:Tuple where N where s20<:N where s12<:T where {T, N}
-#     return CAStyle(Base.BroadcastStyle(Transpose{T,A}), getaxes(A), 2)
-# end
