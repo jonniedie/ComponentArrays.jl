41e4d201b5126b324259797f8964d1fb2db5cfaa
Jonnie Diegelman <jonniediegelman@gmail.com>
2020-07-03 20:24:56 -0400

    Require RecursiveFactorization

--- src/ComponentArrays.jl
+++ src/ComponentArrays.jl
@@ -25,7 +25,9 @@ required(filename) = include(joinpath("if_required", filename))
 
 function __init__()
     @require ConstructionBase="187b0558-2788-49d3-abe0-74a17ed4e7c9" required("constructionbase.jl")
-    @require DiffEqBase="2b5f629d-d688-5b77-993f-72d75c75574e" required("diffeqbase.jl")
+    @require DiffEqBase="2b5f629d-d688-5b77-993f-72d75c75574e" begin
+        @require RecursiveFactorization="f2c3362d-daeb-58d1-803e-2bc74f2840b4" required("diffeqbase.jl")
+    end
     @require RecursiveArrayTools="731186ca-8d62-57ce-b412-fbd966d074cd" required("recursivearraytools.jl")
     @require StaticArrays="90137ffa-7385-5640-81b9-e52037218182" required("staticarrays.jl")
     @require Zygote="e88e6eb3-aa80-5325-afca-941959d7151f" required("zygote.jl")
