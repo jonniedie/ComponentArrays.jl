2e56f904961db460e3c3ee99ebbf738578976ac2
Jonnie Diegelman <jonniediegelman@gmail.com>
2020-07-15 17:45:21 -0400

    added convert method for Zygote

--- src/if_required/zygote.jl
+++ src/if_required/zygote.jl
@@ -16,4 +16,8 @@ Zygote.@adjoint getaxes(x::ComponentArray) = getaxes(x), Δ->ComponentArray(getd
 
 Zygote.@adjoint ComponentArray(data, axes) = ComponentArray(data, axes), Δ->(getdata(Δ), getaxes(Δ))
 
+Zygote.@adjoint function Base.convert(::Type{CA}, x::ComponentArray) where CA<:ComponentArray
+    return ComponentArray(getdata(x), getaxes(CA)), Δ->(CA, ComponentArray(getdata(Δ), getaxes(x)))
+end
+
 Zygote.refresh()
