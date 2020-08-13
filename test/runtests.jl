84bf6f9ec94cd693a5006bc7ffe717b9005aa955
Jonnie Diegelman <jonniediegelman@gmail.com>
2020-08-13 14:15:57 -0400

    Fixed test for partition

--- test/runtests.jl
+++ test/runtests.jl
@@ -41,7 +41,7 @@ _a, _b, _c = fastindices(:a, :b, :c)
     @test fastindices(("a", Val(:b))) == (Val(:a), Val(:b))
 
     @test collect(ComponentArrays.partition(collect(1:12), 3)) == [[1,2,3], [4,5,6], [7,8,9], [10,11,12]]
-    @test size(ComponentArrays.partition(zeros(2,2,2), 1, 2, 2)[2,1,1]) == (1, 2, 2)
+    @test size(collect(ComponentArrays.partition(zeros(2,2,2), 1, 2, 2))[2,1,1]) == (1, 2, 2)
 end
 
 @testset "Construction" begin
