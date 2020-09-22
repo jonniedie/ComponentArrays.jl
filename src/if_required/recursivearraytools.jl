ec0c4070d55179b9879105f0844aae55cfdcaaec
Jonnie Diegelman <jonniediegelman@gmail.com>
2020-09-22 09:26:52 -0400

    Fixed Array(vca::VectorOfArray{<:ComponentArray})

--- src/if_required/recursivearraytools.jl
+++ src/if_required/recursivearraytools.jl
@@ -1,5 +1,5 @@
 AVOA = RecursiveArrayTools.AbstractVectorOfArray
 
 function Base.Array(VA::AVOA{T,N,A}) where {T,N,A<:AbstractVector{<:ComponentVector}}
-    return ComponentArray(reduce(hcat, VA.u)', FlatAxis(), getaxes(VA.u[1])[1])
+    return ComponentArray(reduce(hcat, VA.u), FlatAxis(), getaxes(VA.u[1])[1])
 end
