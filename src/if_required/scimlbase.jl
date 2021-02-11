9658b0cae4783695a39e74450bb4082789d1d134
Jonnie Diegelman <jonniediegelman@gmail.com>
2021-02-10 22:35:54 -0500

    Fixed missing type parameter in scimlbase

--- src/if_required/scimlbase.jl
+++ src/if_required/scimlbase.jl
@@ -1,7 +1,7 @@
 # ComponentSol{T,N,C} = SciMLBase.AbstractODESolution{T,N,C} where C<:AbstractVector{<:ComponentArray}
 
 # Plotting stuff
-function SciMLBase.getsyms(sol::SciMLBase.AbstractODESolution{T,N,C}) where C<:AbstractVector{<:ComponentArray}
+function SciMLBase.getsyms(sol::SciMLBase.AbstractODESolution{T,N,C}) where {T,N,C<:AbstractVector{<:ComponentArray}}
     if SciMLBase.has_syms(sol.prob.f)
         return sol.prob.f.syms
     else
