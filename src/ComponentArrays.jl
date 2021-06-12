e3bd6d02395adb0364db33b30ba3aaf5f73133b7
Jonnie Diegelman <jonniediegelman@gmail.com>
2021-06-12 17:03:26 -0400

    Make ChainRulesCore a direct dependency

--- src/ComponentArrays.jl
+++ src/ComponentArrays.jl
@@ -1,5 +1,7 @@
 module ComponentArrays
 
+import ChainRulesCore
+
 using ArrayInterface
 using LinearAlgebra
 using Requires
@@ -41,8 +43,10 @@ include("show.jl")
 include("plot_utils.jl")
 export labels, label2index
 
+include("compat/chainrulescore.jl")
 
-required(filename) = include(joinpath("if_required", filename))
+
+required(filename) = include(joinpath("compat", filename))
 
 function __init__()
     @require ConstructionBase="187b0558-2788-49d3-abe0-74a17ed4e7c9" required("constructionbase.jl")
@@ -52,7 +56,6 @@ function __init__()
     @require SciMLBase="0bca4576-84f4-4d90-8ffe-ffa030f20462" required("scimlbase.jl")
     @require RecursiveArrayTools="731186ca-8d62-57ce-b412-fbd966d074cd" required("recursivearraytools.jl")
     @require StaticArrays="90137ffa-7385-5640-81b9-e52037218182" required("staticarrays.jl")
-    @require ChainRulesCore="d360d2e6-b24c-11e9-a2a3-2a2ae2dbcce4" required("chainrulescore.jl")
     @require ReverseDiff="37e2e3b7-166d-5795-8a7a-e32c996b4267" required("reversediff.jl")
 end
 
