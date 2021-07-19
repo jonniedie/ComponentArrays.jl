a7b2027d0c33932388c98ef1288d29615c3a0a30
Jonnie Diegelman <jonniediegelman@gmail.com>
2021-07-19 16:43:19 -0400

    Removed more dead broadcasting code

--- src/broadcasting.jl
+++ src/broadcasting.jl
@@ -1,20 +1,16 @@
-const BC = Base.Broadcast
-
-Base.BroadcastStyle(::Type{<:ComponentArray{T, N, A, Axes}}) where {T, N, A, Axes} = BC.BroadcastStyle(A)
-
-Base.getindex(bc::BC.Broadcasted, inds::ComponentIndex...) = bc[value.(inds)...]
+Base.BroadcastStyle(::Type{<:ComponentArray{T, N, A, Axes}}) where {T, N, A, Axes} = Broadcast.BroadcastStyle(A)
 
 # Need special case here for adjoint vectors in order to avoid type instability in axistype
-BC.combine_axes(a::ComponentArray, b::AdjOrTransComponentVector) = (axes(a)[1], axes(b)[2])
-BC.combine_axes(a::AdjOrTransComponentVector, b::ComponentArray) = (axes(b)[2], axes(a)[1])
+Broadcast.combine_axes(a::ComponentArray, b::AdjOrTransComponentVector) = (axes(a)[1], axes(b)[2])
+Broadcast.combine_axes(a::AdjOrTransComponentVector, b::ComponentArray) = (axes(b)[2], axes(a)[1])
 
-BC.axistype(a::CombinedAxis, b::AbstractUnitRange) = a
-BC.axistype(a::AbstractUnitRange, b::CombinedAxis) = b
-BC.axistype(a::CombinedAxis, b::CombinedAxis) = CombinedAxis(FlatAxis(), Base.Broadcast.axistype(_array_axis(a), _array_axis(b)))
-BC.axistype(a::T, b::T) where {T<:CombinedAxis} = a
+Broadcast.axistype(a::CombinedAxis, b::AbstractUnitRange) = a
+Broadcast.axistype(a::AbstractUnitRange, b::CombinedAxis) = b
+Broadcast.axistype(a::CombinedAxis, b::CombinedAxis) = CombinedAxis(FlatAxis(), Base.Broadcast.axistype(_array_axis(a), _array_axis(b)))
+Broadcast.axistype(a::T, b::T) where {T<:CombinedAxis} = a
 
-Base.promote_shape(a::Tuple{Vararg{CombinedAxis}}, b::NTuple{N,AbstractUnitRange}) where N = Base.promote_shape(_array_axis.(a), b)
-Base.promote_shape(a::NTuple{N,AbstractUnitRange}, b::Tuple{Vararg{CombinedAxis}}) where N = Base.promote_shape(a, _array_axis.(b))
+Base.promote_shape(a::Tuple{Vararg{CombinedAxis}}, b::NTuple{N,AbstractUnitRange}) where {N} = Base.promote_shape(_array_axis.(a), b)
+Base.promote_shape(a::NTuple{N,AbstractUnitRange}, b::Tuple{Vararg{CombinedAxis}}) where {N} = Base.promote_shape(a, _array_axis.(b))
 Base.promote_shape(a::Tuple{Vararg{CombinedAxis}}, b::Tuple{Vararg{CombinedAxis}}) = Base.promote_shape(_array_axis.(a), _array_axis.(b))
 Base.promote_shape(a::T, b::T) where {T<:Tuple{Vararg{CombinedAxis}}} = a
 
