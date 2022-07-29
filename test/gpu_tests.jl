9471bef983d0a4c893d49b404f404a2b821cd71e
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2022-07-29 12:10:08 -0600

    test

--- test/gpu_tests.jl
+++ test/gpu_tests.jl
@@ -1,4 +1,5 @@
 using JLArrays
+using ForwardDiff
 
 JLArrays.allowscalar(false)
 
@@ -7,7 +8,7 @@ jlca = ComponentArray(jla, Axis(a=1:2, b=3:4))
 
 @testset "Broadcasting" begin
     @test identity.(jlca + jla) ./ 2 == jlca
-    
+
     @test getdata(map(identity, jlca)) isa JLArray
     @test all(==(0), map(-, jlca, jla))
     @test all(map(-, jlca, jlca) .== 0)
@@ -15,10 +16,14 @@ jlca = ComponentArray(jla, Axis(a=1:2, b=3:4))
 
     @test any(==(1), jlca)
     @test count(>(2), jlca) == 2
-    
+
     # Make sure mapreducing multiple arrays works
     @test mapreduce(==, +, jlca, jla) == 4
     @test mapreduce(abs2, +, jlca) == 30
 
     @test all(map(sin, jlca) .== sin.(jlca) .== sin.(jla) .≈ sin.(1:4))
+end
+
+@testset "ForwardDiff" begin
+    ForwardDiff.gradient(sum, jlca) == one.(jlca)
 end
