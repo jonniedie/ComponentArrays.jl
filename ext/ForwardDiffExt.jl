6f9baf468b88aedca2a541c63c9b5685d67014e0
MilkshakeForReal <yicheng.wu@ucalgary.ca>
2023-03-11 14:01:53 -0700

    remove forwarddiffext

--- ext/ForwardDiffExt.jl
+++
@@ -1,8 +0,0 @@
-module ForwardDiffExt
-
-using ComponentArrays
-isdefined(Base, :get_extension) ? (using ForwardDiff) : (using ..ForwardDiff)
-
-ForwardDiff.jacobian(f, x::ComponentArray, args...) = ForwardDiff.jacobian(f, getdata(x), args...)
-
-end
