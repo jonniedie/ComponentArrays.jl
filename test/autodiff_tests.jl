d6549908a89afcc573392b74ba6c357bd7bae31d
Avik Pal <avikpal@mit.edu>
2023-04-06 10:59:19 -0400

    Fix tests: gradients for Int --> Float

--- test/autodiff_tests.jl
+++ test/autodiff_tests.jl
@@ -32,17 +32,9 @@ truth = ComponentArray(a = [32, 48], x = 156)
         @test zygote_full ≈ truth
     end
 
-    # Not sure why this doesn't work in v1.2, but I don't want to drop the tests for that just
-    # for this to work
-    if VERSION ≥ v"1.6"
-        @test ComponentArray(x=4,) == Zygote.gradient(ComponentArray(x=2,)) do c
-            (;c...,).x^2
-        end[1]
-    else
-        @test_skip ComponentArray(x=4,) == Zygote.gradient(ComponentArray(x=2,)) do c
-            (;c...,).x^2
-        end[1]
-    end
+    @test ComponentArray(x=4.0,) ≈ Zygote.gradient(ComponentArray(x=2,)) do c
+        (;c...,).x^2
+    end[1]
 
     # Issue #148
     ps = ComponentArray(;bias = rand(4))
