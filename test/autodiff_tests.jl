6e52246259b76c8312b8ae16d9e907b2402ef5c7
Avik Pal <avikpal@mit.edu>
2023-09-21 19:06:38 -0400

    Add support for Tracker.jl

--- test/autodiff_tests.jl
+++ test/autodiff_tests.jl
@@ -1,7 +1,4 @@
-import FiniteDiff
-import ForwardDiff
-import ReverseDiff
-import Zygote
+import FiniteDiff, ForwardDiff, ReverseDiff, Tracker, Zygote
 
 using Test
 
@@ -16,21 +13,20 @@ ca = ComponentArray(a=[2, 3], x=2.0)
 truth = ComponentArray(a = [32, 48], x = 156)
 
 @testset "$(nameof(F_))" for F_ in (F_idx_val, F_idx_sym, F_view_val, F_view_sym, F_prop)
-    finite = FiniteDiff.finite_difference_gradient(ca -> F_(ca), ca)
+    finite = FiniteDiff.finite_difference_gradient(F_, ca)
     @test finite ≈ truth
 
-    forward = ForwardDiff.gradient(ca -> F_(ca), ca)
+    forward = ForwardDiff.gradient(F_, ca)
     @test forward ≈ truth
 
-    reverse = ReverseDiff.gradient(ca -> F_(ca), ca)
+    reverse = ReverseDiff.gradient(F_, ca)
     @test reverse ≈ truth
 
-    zygote_full = Zygote.gradient(ca -> F_(ca), ca)[1]
-    if F_ == F_prop && VERSION < v"1.3"
-        @test_broken zygote_full ≈ truth
-    else
-        @test zygote_full ≈ truth
-    end
+    tracker = Tracker.gradient(F_, ca)[1]
+    @test tracker ≈ truth
+
+    zygote_full = Zygote.gradient(F_, ca)[1]
+    @test zygote_full ≈ truth
 
     @test ComponentArray(x=4.0,) ≈ Zygote.gradient(ComponentArray(x=2,)) do c
         (;c...,).x^2
