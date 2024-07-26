4fdfe36ea03bf87ebac1997df1c3ce89ce2e3715
Jonnie Diegelman <jonnie.diegelman@flyzipline.com>
2024-07-26 16:33:53 -0400

    Make `haskey` work for `LazyArray`

--- test/runtests.jl
+++ test/runtests.jl
@@ -345,6 +345,9 @@ end
     @test propertynames(ca2) == (:a, :b, :c) # ComponentArray
     @test propertynames(ca2.b) == (:a, :b) # LazyArray
 
+    @test haskey(ca2, :a) # ComponentArray
+    @test haskey(ca2.b, 1) # LazyArray
+
     @test keys(ca2) == (:a, :b, :c)
     @test keys(ca2.b) == Base.OneTo(2)
 end
